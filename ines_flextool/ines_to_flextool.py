65a80c31d5e33e96b253cde0cd64bc7c8d248596
ArttuTupala <arttu.tupala@gmail.com>
2026-02-19 11:13:36 +0200

    allow float profiles p2

--- ines_flextool/ines_to_flextool.py
+++ ines_flextool/ines_to_flextool.py
@@ -150,19 +150,19 @@ def create_profiles(source_db, target_db):
                     profile_limit_upper_realized = param
                     if not any(param["entity_byname"] == forecast_param["entity_byname"] for forecast_param in profile_limit_upper_forecasts):
                         alt_ent_class_target = [param["alternative_name"], (unit+"_upper",), "profile"]
-                        target_db = pass_profile_value(source_db, target_db, param, alt_ent_class_target)
+                        target_db = pass_profile_value(target_db, param, alt_ent_class_target)
             for param in profile_limit_lower:
                 if param["entity_byname"] == source_entity["entity_byname"]:
                     profile_limit_lower_realized = param
                     if not any(param["entity_byname"] == forecast_param["entity_byname"] for forecast_param in profile_limit_lower_forecasts):
                         alt_ent_class_target = [param["alternative_name"], (unit+"_lower",), "profile"]
-                        target_db = pass_profile_value(source_db, target_db, param, alt_ent_class_target)
+                        target_db = pass_profile_value(target_db, param, alt_ent_class_target)
             for param in profile_limit_fix:
                 if param["entity_byname"] == source_entity["entity_byname"]:
                     profile_limit_fix_realized = param
                     if not any(param["entity_byname"] == forecast_param["entity_byname"] for forecast_param in profile_limit_fix_forecasts):
                         alt_ent_class_target = [param["alternative_name"], (unit+"_fix",), "profile"]
-                        target_db = pass_profile_value(source_db, target_db, param, alt_ent_class_target)
+                        target_db = pass_profile_value(target_db, param, alt_ent_class_target)
             for param in profile_limit_upper_forecasts:
                 if param["entity_byname"] == source_entity["entity_byname"]:
                     target_db = create_4d_from_stochastic_interpolation(target_db, param, profile_limit_upper_realized, "profile", [(unit+"_upper",), "profile"], 
@@ -206,15 +206,12 @@ def create_profiles(source_db, target_db):
         print("commit profile parameters error")
     return target_db
 
-def pass_profile_value(source_db, target_db, param, alt_ent_class_target):
+def pass_profile_value(target_db, param, alt_ent_class_target):
     profile = api.from_database(param["value"], param["type"])
     if isinstance(profile, api.TimeSeriesVariableResolution) or isinstance(profile, api.Map):
         value = api.Map([str(x) for x in profile.indexes], [float(x) for x in profile.values])
     elif isinstance(profile,float):
-        print("float profile: creating a map with the same value for all time steps")
-        timeline = source_db.get_parameter_value_items(entity_class_name="system", parameter_definition_name="timeline")[0]
-        timeline_value = api.from_database(timeline["value"], timeline["type"])
-        value = api.Map([str(x) for x in timeline_value.indexes], [profile for x in timeline_value.indexes])
+        value = profile
     target_db = ines_transform.add_item_to_DB(target_db, "profile", alt_ent_class_target, value, value_type='map')
     return target_db
 
